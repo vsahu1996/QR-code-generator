86b94f7781a81ff50472dc0d2c0330bb15ffccb2
vsahu1996 <vsahu228@gmail.com>
2022-09-30 13:07:35 +0530

    modified

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-var qrcode = new QRCode("qrcode");
-
-function makeCode () {
-  var elText = document.getElementById("text");
-  
-  if (!elText.value) {
-    // alert("Input a text");
-    // elText.focus();
-    // return;
-  }
-  
-  qrcode.makeCode(elText.value);
-}
-
-makeCode();
-
-$("#text").on("blur", function () {
-  makeCode();
-}).on("keydown", function (e) {
-  if (e.keyCode == 13) {
-    makeCode();
-  }
+const wrapper = document.querySelector(".wrapper"),
+qrInput = wrapper.querySelector(".form input"),
+generateBtn = wrapper.querySelector(".form button"),
+qrImg = wrapper.querySelector(".qr-code img");
+let preValue;
+generateBtn.addEventListener("click", () => {
+    let qrValue = qrInput.value.trim();
+    if(!qrValue || preValue === qrValue) return;
+    preValue = qrValue;
+    generateBtn.innerText = "Generating QR Code...";
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+    qrImg.addEventListener("load", () => {
+        wrapper.classList.add("active");
+        generateBtn.innerText = "Generate QR Code";
+    });
+});
+qrInput.addEventListener("keyup", () => {
+    if(!qrInput.value.trim()) {
+        wrapper.classList.remove("active");
+        preValue = "";
+    }
 });
